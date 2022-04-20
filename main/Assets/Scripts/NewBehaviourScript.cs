7f43330969fd09597da157c38dfb828b4dfc9229
Nicolaj Frederiksen <72729883+NicolajFrederiksen@users.noreply.github.com>
2022-04-20 12:17:21 +0200

    hej

--- main/Assets/Scripts/NewBehaviourScript.cs
+++ main/Assets/Scripts/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hej christian
     }
 
     // Update is called once per frame
